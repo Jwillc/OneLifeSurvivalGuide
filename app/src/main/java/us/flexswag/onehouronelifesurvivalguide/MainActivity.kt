5dfeddb2d066de5122c663177fe5dea5dc6afc99
James <jameswcampb@gmail.com>
2018-11-23 22:21:09 -0800

    Added more guides.

--- app/src/main/java/us/flexswag/onehouronelifesurvivalguide/MainActivity.kt
+++ app/src/main/java/us/flexswag/onehouronelifesurvivalguide/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private var vidMap = mutableMapOf<Int, String>()
     private var basicsMap = mutableMapOf<Int, Int>()
     private var clothingMap = mutableMapOf<Int, Int>()
+    private var farmingMap = mutableMapOf<Int, Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +34,15 @@ class MainActivity : AppCompatActivity() {
         vidMap[1] = "https://youtu.be/MRRNexVvK1E" // Forging Video
 
         basicsMap[1] = R.drawable.basket // Basket Guide
+        basicsMap[2] = R.drawable.claybowl
+        basicsMap[3] = R.drawable.clayplate
+        basicsMap[4] = R.drawable.fire
+        basicsMap[5] = R.drawable.firebowdrill
+        basicsMap[6] = R.drawable.firebrand
+        basicsMap[7] = R.drawable.hotflatrock
+        basicsMap[8] = R.drawable.kiln
+        basicsMap[9] = R.drawable.kindlingandtinder
+        basicsMap[10] = R.drawable.leaf
 
         clothingMap[1] = R.drawable.rabbitfur
         clothingMap[2] = R.drawable.needlethread
@@ -40,6 +50,18 @@ class MainActivity : AppCompatActivity() {
         clothingMap[4] = R.drawable.rabbitbackpack
         clothingMap[5] = R.drawable.wolfhat
         clothingMap[6] = R.drawable.bowandarrow
+
+        farmingMap[1] = R.drawable.bowlofwater
+        farmingMap[2] = R.drawable.carrot
+        farmingMap[3] = R.drawable.compost
+        farmingMap[4] = R.drawable.deeptilledrow
+        farmingMap[5] = R.drawable.farmingrows
+        farmingMap[6] = R.drawable.gooseberrybush
+        farmingMap[7] = R.drawable.milkweed
+        farmingMap[8] = R.drawable.ripewheat
+        farmingMap[9] = R.drawable.shovelofdung
+        farmingMap[10] = R.drawable.stonehoe
+        farmingMap[11] = R.drawable.straw
 
         foodsMap[1] = R.drawable.obj_carrot
         foodsMap[2] = R.drawable.obj_wildcarrot
@@ -60,10 +82,6 @@ class MainActivity : AppCompatActivity() {
             when (previousDataId) {
                 "main" -> {
                     val newData = jsonToMap("categories", "categoryName")
-                    resetAdapter(newData)
-                }
-                "foods" -> {
-                    val newData = jsonToMap("foods", "foodType")
                     resetAdapter(newData)
                 }
             }
@@ -111,6 +129,11 @@ class MainActivity : AppCompatActivity() {
                 resetAdapter(newData)
                 previousDataId = "main"
             }
+            partItem.id == "farmingCat" -> {
+                val newData = jsonToMap("farming", "farmingTitle")
+                resetAdapter(newData)
+                previousDataId = "main"
+            }
 
             // Videos
             partItem.id == "vidForge" -> {
@@ -119,6 +142,33 @@ class MainActivity : AppCompatActivity() {
             // Basics Category
             partItem.id == "basicsBasket" -> {
                 launchDetailActivity(basicsMap[1]!!)
+            }
+            partItem.id == "basicsClayBowl" -> {
+                launchDetailActivity(basicsMap[2]!!)
+            }
+            partItem.id == "basicsClayPlate" -> {
+                launchDetailActivity(basicsMap[3]!!)
+            }
+            partItem.id == "basicsFire" -> {
+                launchDetailActivity(basicsMap[4]!!)
+            }
+            partItem.id == "basicsFireBowDrill" -> {
+                launchDetailActivity(basicsMap[5]!!)
+            }
+            partItem.id == "basicsFireBrand" -> {
+                launchDetailActivity(basicsMap[6]!!)
+            }
+            partItem.id == "basicsHotFlatRock" -> {
+                launchDetailActivity(basicsMap[7]!!)
+            }
+            partItem.id == "basicsAdobeKiln" -> {
+                launchDetailActivity(basicsMap[8]!!)
+            }
+            partItem.id == "basicsKindlingAndTinder" -> {
+                launchDetailActivity(basicsMap[9]!!)
+            }
+            partItem.id == "basicsLeaf" -> {
+                launchDetailActivity(basicsMap[10]!!)
             }
             // Food Category
             partItem.id == "foodCarrot" -> {
@@ -148,6 +198,40 @@ class MainActivity : AppCompatActivity() {
             }
             partItem.id == "clothingBowArrow" -> {
                 launchDetailActivity(clothingMap[6]!!)
+            }
+            // Farming Category
+            partItem.id == "farmingBowlOfWater" -> {
+                launchDetailActivity(farmingMap[1]!!)
+            }
+            partItem.id == "farmingCarrot" -> {
+                launchDetailActivity(farmingMap[2]!!)
+            }
+            partItem.id == "farmingCompost" -> {
+                launchDetailActivity(farmingMap[3]!!)
+            }
+            partItem.id == "farmingDeepTilledRow" -> {
+                launchDetailActivity(farmingMap[4]!!)
+            }
+            partItem.id == "farmingRows" -> {
+                launchDetailActivity(farmingMap[5]!!)
+            }
+            partItem.id == "farmingGooseBerryBush" -> {
+                launchDetailActivity(farmingMap[6]!!)
+            }
+            partItem.id == "farmingMilkweed" -> {
+                launchDetailActivity(farmingMap[7]!!)
+            }
+            partItem.id == "farmingRipeWheat" -> {
+                launchDetailActivity(farmingMap[8]!!)
+            }
+            partItem.id == "farmingShovelOfDung" -> {
+                launchDetailActivity(farmingMap[9]!!)
+            }
+            partItem.id == "farmingStoneHoe" -> {
+                launchDetailActivity(farmingMap[10]!!)
+            }
+            partItem.id == "farmingStraw" -> {
+                launchDetailActivity(farmingMap[11]!!)
             }
 
             else -> Snackbar.make(this.currentFocus, "Clicked: " + partItem.id, Snackbar.LENGTH_LONG)
